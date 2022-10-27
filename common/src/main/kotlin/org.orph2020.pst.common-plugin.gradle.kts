8152c368bd10bbc279af77a6de29d15779585f30
Paul Harrison <paul.harrison@manchester.ac.uk>
2022-10-27 20:40:10 +0100

    add some global build properties

--- common/src/main/kotlin/org.orph2020.pst.common-plugin.gradle.kts
+++ common/src/main/kotlin/org.orph2020.pst.common-plugin.gradle.kts
@@ -1,3 +1,4 @@
+import io.quarkus.gradle.tasks.QuarkusTask
 
 plugins {// even after all that above the variable is not available in scope https://stackoverflow.com/questions/60165254/how-to-parametrize-kotlin-version-in-the-plugins-block-of-a-build-gradle-kts-scr
     java
@@ -21,7 +22,6 @@ dependencies {
 }
 
 
-
 java {
     sourceCompatibility = JavaVersion.VERSION_11
     targetCompatibility = JavaVersion.VERSION_11
@@ -30,6 +30,15 @@ java {
 tasks.withType<JavaCompile> {
     options.encoding = "UTF-8"
     options.compilerArgs.add("-parameters")
+}
+// https://quarkus.io/guides/gradle-tooling https://quarkus.io/guides/config-reference
+// set some global quarkus properties here - note that the systemProperty() call
+// recommended in first of above does not seem to work - so doing the more obvious direct java call
+// note also that these are build time properties - run time stuff should still go into each application.properties.
+tasks.withType<QuarkusTask> {
+    System.setProperty("quarkus.container-image.registry","130.88.9.40:9083") // http://redmine.jb.man.ac.uk/issues/6457
+    System.setProperty("quarkus.kubernetes.part-of","proposalTool")
+    System.setProperty("quarkus.container-image.group", "orppst")
 }
 
 tasks.test {
