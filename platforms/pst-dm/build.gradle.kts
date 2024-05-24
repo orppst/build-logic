07d80df777fa1b33044f113be180c9845899b312
Paul Harrison <paul.harrison@manchester.ac.uk>
2024-05-24 14:59:33 +0100

    update to latest ProposalDM

--- platforms/pst-dm/build.gradle.kts
+++ platforms/pst-dm/build.gradle.kts
@@ -1,6 +1,5 @@
 plugins {
     id("java-platform")
-
 }
 
 group = "org.orph2020.pst.platforms"
@@ -8,6 +7,6 @@ javaPlatform.allowDependencies()
 
 
 dependencies {
-    api("org.javastro.ivoa.dm:proposaldm:0.4.1-SNAPSHOT:quarkus")//IMPL special dependency that does not include the
-    api("org.javastro.ivoa.dm:proposaldm:0.4.1-SNAPSHOT:test") // FIXME will want to remove for deployment
+    api("org.javastro.ivoa.dm:proposaldm:0.4.2-SNAPSHOT:quarkus")//IMPL special dependency that does not include the
+    api("org.javastro.ivoa.dm:proposaldm:0.4.2-SNAPSHOT:test") // FIXME will want to remove for deployment
 }
--- platforms/quarkus-base/build.gradle.kts
+++ platforms/quarkus-base/build.gradle.kts
@@ -15,7 +15,7 @@ javaPlatform.allowDependencies()
 dependencies {
     constraints {
         api(enforcedPlatform("io.quarkus.platform:quarkus-bom:3.7.3"))
-        api("org.javastro.ivoa.dm:proposaldm:0.4.1-SNAPSHOT:quarkus")//IMPL special dependency that does not include the
+        api("org.javastro.ivoa.dm:proposaldm:0.4.2-SNAPSHOT:quarkus")//IMPL special dependency that does not include the
 
     }
     api(enforcedPlatform("io.quarkus.platform:quarkus-bom:3.7.3"))
