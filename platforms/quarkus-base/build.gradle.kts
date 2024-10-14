486e0b48b02323a0e0e6f686af48626abb167d2a
Paul Harrison <paul.harrison@manchester.ac.uk>
2024-10-14 07:45:30 +0100

    upgrade to quarkus 3.13.3

--- platforms/quarkus-base/build.gradle.kts
+++ platforms/quarkus-base/build.gradle.kts
@@ -14,9 +14,9 @@ javaPlatform.allowDependencies()
 
 dependencies {
     constraints {
-        api(enforcedPlatform("io.quarkus.platform:quarkus-bom:3.12.2"))
+        api(enforcedPlatform("io.quarkus.platform:quarkus-bom:3.13.3"))
         api("org.javastro.ivoa.dm:proposaldm:0.5.1-SNAPSHOT:quarkus")//IMPL special dependency that does not include the
 
     }
-    api(enforcedPlatform("io.quarkus.platform:quarkus-bom:3.12.2"))
+    api(enforcedPlatform("io.quarkus.platform:quarkus-bom:3.13.3"))
 }
