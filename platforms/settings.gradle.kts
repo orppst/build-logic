a3c818b088bdb48ce2fb9d27fbfe1b5abacb1eb2
Paul Harrison <paul.harrison@manchester.ac.uk>
2025-07-18 15:03:27 +0100

    try to force the repository management for snapshots

--- platforms/settings.gradle.kts
+++ platforms/settings.gradle.kts
@@ -3,7 +3,6 @@ dependencyResolutionManagement {
     repositories {
         mavenCentral()
         mavenLocal()
-        mavenLocal()
         /*
         add this repository to pick up the SNAPSHOT version of the IVOA base library - in the future this
         will not be necessary when this library is released as a non-SNAPSHOT version.
