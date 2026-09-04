f5b366847121a6fc01f6f73e2e3fd18b3c00dd33
Paul Harrison <paul.harrison@manchester.ac.uk>
2026-09-04 11:54:47 +0100

    update to latest LTS quarkus

--- common/src/main/kotlin/org.orph2020.pst.common-plugin.gradle.kts
+++ common/src/main/kotlin/org.orph2020.pst.common-plugin.gradle.kts
@@ -27,7 +27,7 @@ group = "org.orph2020.pst"
 
 dependencies {
 
-    implementation(enforcedPlatform("io.quarkus.platform:quarkus-bom:3.33.1"))
+    implementation(enforcedPlatform("io.quarkus.platform:quarkus-bom:3.33.3.2"))
     implementation("io.quarkus:quarkus-container-image-docker") // or perhaps below is better - esp if you do not have docker installed... use docker for now because of permissions
 //   implementation("io.quarkus:quarkus-container-image-jib")
     implementation("io.quarkus:quarkus-kubernetes")
--- platforms/pst-base/build.gradle.kts
+++ platforms/pst-base/build.gradle.kts
@@ -8,7 +8,7 @@ group = "org.orph2020.pst.platforms"
 
 dependencies {
     constraints {
-        api(enforcedPlatform("io.quarkus.platform:quarkus-bom:3.33.1"))
+        api(enforcedPlatform("io.quarkus.platform:quarkus-bom:3.33.3.2"))
         api("org.javastro.ivoa.dm:proposaldm:0.7.1-SNAPSHOT")
     }
 }
