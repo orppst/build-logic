2c2052e581d319b0b8b3971182f78cde147c921d
Paul Harrison <paul.harrison@manchester.ac.uk>
2026-09-11 17:56:29 +0100

    update to later proposalDM
    still does not fix all the hibernate function descriptions....

--- platforms/pst-base/build.gradle.kts
+++ platforms/pst-base/build.gradle.kts
@@ -9,6 +9,6 @@ group = "org.orph2020.pst.platforms"
 dependencies {
     constraints {
         api(enforcedPlatform("io.quarkus.platform:quarkus-bom:3.33.3.2"))
-        api("org.javastro.ivoa.dm:proposaldm:0.7.1-SNAPSHOT")
+        api("org.javastro.ivoa.dm:proposaldm:0.7.3-SNAPSHOT")
     }
 }
